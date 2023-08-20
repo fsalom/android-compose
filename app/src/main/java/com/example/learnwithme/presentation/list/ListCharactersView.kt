991404cbf8cdf09e28978a71fe0437ffb36c95a5
Fernando Salom <fer@rudo.es>
2023-08-20 16:54:54 +0200

    Add new list view

--- app/src/main/java/com/example/learnwithme/presentation/list/ListCharactersView.kt
+++ app/src/main/java/com/example/learnwithme/presentation/list/ListCharactersView.kt
@@ -3,11 +3,13 @@ import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +19,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.learnwithme.domain.entity.Character
@@ -28,12 +32,10 @@ fun ListCharactersView(viewModel: ListCharactersViewModelInterface) {
     if (scrollState.isScrollInProgress){
         println("scrolling: " + scrollState.value)
     }
-    viewModel.load()
-    //LaunchedEffect(uisState) {
 
-    //}
     if (uisState.isLoading) {
         CircularProgressIndicator()
+        viewModel.load()
     }
     if (uisState.items.isNotEmpty()) {
         MessageList(scrollState = scrollState, messages = uisState.items)
@@ -52,13 +54,20 @@ fun MessageList(scrollState: ScrollState, messages: List<Character>) {
 
 @Composable
 fun CharacterRow(character: Character) {
-    Row(modifier = Modifier.padding(all = 10.dp)) {
-        AsyncImage(model = character.image, contentDescription = character.name + "image", modifier = Modifier
-            .size(64.dp)
-            .clip(CircleShape)
-            .border(2.dp, Color.Gray, CircleShape) )
-
-        Text(text = character.name)
+    Card(modifier = Modifier.fillMaxWidth().padding(10.dp)) {
+        Row(modifier = Modifier.padding(all = 10.dp)) {
+            AsyncImage(
+                model = character.image,
+                contentDescription = character.name + "image",
+                modifier = Modifier
+                    .size(64.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.Gray, CircleShape)
+            )
+            Column(modifier = Modifier.padding(all = 10.dp)) {
+                Text(text = character.name, style = TextStyle(fontWeight = FontWeight.Bold, fontSize = ))
+                Text(text = character.species)
+            }
+        }
     }
-
 }
