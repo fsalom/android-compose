4e3ebabfdc6e4ee1ea1649f3bc9578d1591d6b63
Fernando Salom <fer@rudo.es>
2023-08-24 16:27:18 +0200

    Fix view and viewModel for characters list

--- app/src/main/java/com/example/learnwithme/presentation/list/ListCharactersView.kt
+++ app/src/main/java/com/example/learnwithme/presentation/list/ListCharactersView.kt
@@ -1,19 +1,15 @@
 package com.example.learnwithme.presentation.list
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
@@ -21,6 +17,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -31,40 +30,65 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.learnwithme.domain.entity.Character
+import kotlinx.coroutines.delay
 
 @Composable
 fun ListCharactersView(viewModel: ListCharactersViewModelInterface) {
     val uiState by viewModel.uiState.collectAsState()
-    val scrollState = rememberLazyListState()
-
 
     if (uiState.isLoading) {
-        CircularProgressIndicator()
+        CustomProgressIndicator()
         viewModel.load()
     }
 
-        InfiniteScroll(itemCount = uiState.items.size,
-            loadMoreItems = {
-                viewModel.load()
+    InfiniteScroll(itemCount = uiState.items.size,
+        loadMoreItems = {
+            viewModel.load()
         }) {
-            CharacterRow(uiState.items[it])
+            CharacterRow(uiState.items[it.first])
+            if (it.second) {
+                CustomProgressIndicator()
+            }
         }
 }
 
 @Composable
+fun CustomProgressIndicator() {
+    var show by remember { mutableStateOf(true) }
+    val timeout: Long = 10000
+    LaunchedEffect(key1 = Unit){
+        delay(timeout)
+        show = false
+    }
+    if (show) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier.size(32.dp),
+                color = Color.LightGray,
+                strokeWidth = 5.dp
+            )
+        }
+    }
+}
+@Composable
 fun InfiniteScroll(
     itemCount: Int,
     loadMoreItems: () -> Unit,
-    content: @Composable (Int) -> Unit
+    content: @Composable (Pair<Int, Boolean>) -> Unit
 ) {
     val listState = rememberLazyListState()
 
     LazyColumn(state = listState) {
         items(itemCount) { index ->
-            content(index)
-            if (index == itemCount - 1) {
+            var isLoading = index == itemCount - 1
+            if (isLoading) {
                 loadMoreItems()
             }
+            content(Pair(index, isLoading))
         }
     }
 }
--- app/src/main/java/com/example/learnwithme/presentation/list/ListCharactersViewModel.kt
+++ app/src/main/java/com/example/learnwithme/presentation/list/ListCharactersViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.learnwithme.domain.usecase.CharacterUseCaseInterface
 import com.example.learnwithme.domain.entity.Character
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -13,7 +14,6 @@ import kotlinx.coroutines.launch
 interface ListCharactersViewModelInterface {
     fun load()
     val uiState: StateFlow<CharactersUiState>
-    val isRefresh: Boolean
 }
 
 data class CharactersUiState(
@@ -25,20 +25,14 @@ class ListCharactersViewModel(private val useCase: CharacterUseCaseInterface):
     ListCharactersViewModelInterface,
     ViewModel() {
     private var page = 1
-    override val isRefresh: Boolean = false
 
     private val _uiState = MutableStateFlow(CharactersUiState(isLoading = true))
     override val uiState: StateFlow<CharactersUiState> = _uiState.asStateFlow()
 
     override fun load() {
         viewModelScope.launch {
-            _uiState.update {
-                it.copy(
-                    isLoading = true
-                )
-            }
-            _uiState.emit(uiState.value)
             val result = useCase.getNextPageAndCharacters(page)
+            delay(3000)
             page += if (result.first) 1 else 0
             _uiState.update {
                 it.copy(
