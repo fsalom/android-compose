75b8201c7f2aa691e1fc97ae25d0b4313da35bb6
Fernando Salom <fer@rudo.es>
2023-09-04 13:05:06 +0200

    Fix problem with repository import

--- app/src/main/java/com/example/learnwithme/data/repository/CharacterRepository.kt
+++ app/src/main/java/com/example/learnwithme/data/repository/CharacterRepository.kt
@@ -2,6 +2,7 @@ package com.example.learnwithme.data.repository
 
 import com.example.learnwithme.data.datasource.CharacterDataSourceInterface
 import com.example.learnwithme.domain.entity.Pagination
+import com.example.learnwithme.domain.entity.Character
 
 class CharacterRepository(val dataSource: CharacterDataSourceInterface): CharacterRepositoryInterface {
     override suspend fun getPagination(page: Int): Pagination {
