3ea6de7916031313e01e84610f2696ca654de7ae
Fernando Salom <fer@rudo.es>
2023-09-07 05:16:06 +0200

    Add detail view design

--- app/src/main/java/com/example/learnwithme/presentation/detail/DetailCharacterView.kt
+++ app/src/main/java/com/example/learnwithme/presentation/detail/DetailCharacterView.kt
@@ -1,9 +1,23 @@
 package com.example.learnwithme.presentation.detail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.learnwithme.presentation.list.CustomProgressIndicator
 
 @Composable
@@ -18,6 +32,21 @@ fun DetailCharactersView(
     }
 
     uiState.character?.let {
-        Text(it.name)
+        Column {
+            AsyncImage(
+                model = it.image,
+                contentDescription = it.name + "image",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentScale = ContentScale.FillWidth
+            )
+            Text(it.name,
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                    color = Color.Black
+                )
+            )
+        }
     }
 }
