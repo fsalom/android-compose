5741b26babf7119128f8c53f09558b04873e203d
Fernando Salom <fer@rudo.es>
2023-09-29 22:07:32 +0200

    Rename customview to component

--- chapter-final/app/src/main/java/com/example/learnwithme/presentation/components/CustomProgressIndicator.kt
+++ chapter-final/app/src/main/java/com/example/learnwithme/presentation/components/CustomProgressIndicator.kt
@@ -1,4 +1,4 @@
-package com.example.learnwithme.presentation.custonviews
+package com.example.learnwithme.presentation.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
--- chapter-final/app/src/main/java/com/example/learnwithme/presentation/detail/DetailCharacterView.kt
+++ chapter-final/app/src/main/java/com/example/learnwithme/presentation/detail/DetailCharacterView.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import com.example.learnwithme.presentation.custonviews.CustomProgressIndicator
+import com.example.learnwithme.presentation.components.CustomProgressIndicator
 
 @Composable
 fun DetailCharactersView(
