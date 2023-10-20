937f89100dd2d052077d18e22990fecd13d0f174
Fernando Salom <fer@rudo.es>
2023-10-20 18:11:07 -0500

    Add character entity and dao

--- chapter-final/app/src/main/java/com/example/learnwithme/data/datasource/character/database/dbo/CharacterDBO.kt
+++ chapter-final/app/src/main/java/com/example/learnwithme/data/datasource/character/database/dbo/CharacterDBO.kt
@@ -2,12 +2,13 @@ package com.example.learnwithme.data.datasource.character.database.dbo
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.learnwithme.manager.database.BaseEntity
 
 @Entity(tableName = "Characters")
-data class CharacterDBO(
-    @PrimaryKey val id: Int,
+data class CharacterEntity(
     val name: String? = null,
     val status: String? = null,
     val species: String? = null,
-    val image: String? = null
-)
+    val image: String? = null,
+    override val id: Int
+) : BaseEntity()
