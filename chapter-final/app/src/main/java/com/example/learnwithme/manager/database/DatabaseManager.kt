985f9cd2b59701c18da45ce5000461cd51b5fda8
Fernando Salom <fer@rudo.es>
2023-10-22 20:07:28 +0200

    Add new functions to database manager

--- chapter-final/app/src/main/java/com/example/learnwithme/manager/database/DatabaseManager.kt
+++ chapter-final/app/src/main/java/com/example/learnwithme/manager/database/DatabaseManager.kt
@@ -1,10 +1,12 @@
 package com.example.learnwithme.manager.database
 
 import androidx.annotation.NonNull
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ComputableLiveData
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.room.Dao
+import androidx.room.Database
 import androidx.room.Delete
 import androidx.room.Entity
 import androidx.room.Insert
@@ -13,9 +15,11 @@ import androidx.room.OnConflictStrategy
 import androidx.room.PrimaryKey
 import androidx.room.RawQuery
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import androidx.room.Update
 import androidx.sqlite.db.SimpleSQLiteQuery
 import androidx.sqlite.db.SupportSQLiteQuery
+import com.example.learnwithme.data.datasource.character.database.query.CharacterDao
 
 abstract class BaseDao<T : BaseEntity>(
     private val tableName: String,
@@ -41,6 +45,9 @@ abstract class BaseDao<T : BaseEntity>(
 
     @RawQuery
     protected abstract fun deleteAll(query: SupportSQLiteQuery): Int
+
+    @RawQuery
+    protected abstract fun getAll(query: SupportSQLiteQuery): List<T>
 
     fun deleteAll() {
         val query = SimpleSQLiteQuery("DELETE FROM $tableName")
@@ -74,6 +81,12 @@ abstract class BaseDao<T : BaseEntity>(
         return resultLiveData
     }
 
+
+    fun getAll(): List<T> {
+        val query = SimpleSQLiteQuery("SELECT * FROM $tableName")
+        return getAll(query)
+    }
+
     fun getEntity(ids: List<Int>): LiveData<List<T>?> {
 
         return object : ComputableLiveData<List<T>>() {
@@ -95,6 +108,5 @@ abstract class BaseDao<T : BaseEntity>(
 }
 
 abstract class BaseEntity {
-
     abstract val id: Int
 }
