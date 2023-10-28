661a4880e48d445668732235f914794d027e56ca
Fernando Salom <fer@rudo.es>
2023-10-28 16:24:45 +0200

    Add room implementation

---
+++ chapter-final/app/src/main/java/com/example/learnwithme/data/datasource/character/database/room/RoomCharacterDataSource.kt
@@ -0,0 +1,50 @@
+package com.example.learnwithme.data.datasource.character.database.room
+
+import com.example.learnwithme.data.datasource.character.database.CharacterDatabaseDataSourceInterface
+import com.example.learnwithme.data.datasource.character.database.room.dbo.CharacterEntity
+import com.example.learnwithme.data.datasource.character.database.room.dbo.toDomain
+import com.example.learnwithme.data.datasource.character.database.room.query.CharacterDao
+import com.example.learnwithme.domain.entity.Character
+import com.example.learnwithme.domain.entity.Pagination
+
+class RoomCharacterDataSource(
+    val dao: CharacterDao
+): CharacterDatabaseDataSourceInterface {
+    override suspend fun getFavorites(): List<Character> {
+        var charactersEntity = dao.getFavorites()
+        return charactersEntity.map { it.toDomain() }
+    }
+
+    override suspend fun getPagination(page: Int): Pagination {
+        var charactersEntity = dao.getCharacters(page)
+        return Pagination(characters = charactersEntity.map { it.toDomain() }, hasNextPage = true)
+    }
+
+    override suspend fun search(text: String): List<Character> {
+        var charactersEntity = dao.searchCharacters(text)
+        return charactersEntity.map { it.toDomain() }
+    }
+
+    override suspend fun getCharacterWith(id: Int): Character? {
+        var character = dao.getEntitySync(id)
+        if (character != null) {
+            return character.toDomain()
+        }
+        return null
+    }
+
+    override suspend fun save(characters: List<Character>, page: Int) {
+        var characters = characters
+        for (character in characters) {
+            dao.insertOrReplace(CharacterEntity.create(character = character, page = page))
+        }
+    }
+
+    override suspend fun favOrUnFav(character: Character) {
+        var character = dao.getEntitySync(character.id)
+        if (character != null) {
+            character.isFavorite = !character.isFavorite
+            dao.insertOrReplace(character)
+        }
+    }
+}
