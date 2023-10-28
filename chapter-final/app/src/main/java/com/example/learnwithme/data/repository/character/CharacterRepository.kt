f93b24707572a000e9e8394ca8f77cffe353b208
Fernando Salom <fer@rudo.es>
2023-10-28 16:22:55 +0200

    Add character repository logic

---
+++ chapter-final/app/src/main/java/com/example/learnwithme/data/repository/character/CharacterRepository.kt
@@ -0,0 +1,59 @@
+package com.example.learnwithme.data.repository.character
+
+import com.example.learnwithme.data.datasource.character.database.CharacterDatabaseDataSourceInterface
+import com.example.learnwithme.data.datasource.character.remote.CharacterRemoteDataSourceInterface
+import com.example.learnwithme.domain.entity.Pagination
+import com.example.learnwithme.domain.entity.Character
+
+class CharacterRepository(
+    private val remoteDataSource: CharacterRemoteDataSourceInterface,
+    private val databaseDatasource: CharacterDatabaseDataSourceInterface
+): CharacterRepositoryInterface {
+
+    private val MILISECONDS_TO_UPDATE: Long = 300000
+    override suspend fun getPagination(page: Int): Pagination {
+        var localCharacters = databaseDatasource.getPagination(page)
+        return if (localCharacters.characters.isEmpty()) {
+            var pagination = remoteDataSource.getPagination(page)
+            databaseDatasource.save(pagination.characters, page = page)
+            pagination
+        } else {
+            if (shouldBeUpdated(localCharacters.characters.first().creationDate)) {
+                var pagination = remoteDataSource.getPagination(page)
+                databaseDatasource.save(pagination.characters, page = page)
+                pagination
+            } else {
+                localCharacters
+            }
+        }
+    }
+
+    override suspend fun getPaginationFor(text: String, page: Int): Pagination {
+        return remoteDataSource.getPaginationFor(text, page)
+    }
+
+    override suspend fun getCharacterWith(id: Int): Character? {
+        var localCharacter = databaseDatasource.getCharacterWith(id)
+        return if (localCharacter == null) {
+            remoteDataSource.getCharacterWith(id)
+        } else {
+            localCharacter
+        }
+    }
+
+    override suspend fun favOrUnFav(character: Character) {
+        databaseDatasource.favOrUnFav(character)
+    }
+
+    override suspend fun getFavoriteCharacters(): List<Character> {
+        return databaseDatasource.getFavorites()
+    }
+
+    private fun shouldBeUpdated(creationTime: Long): Boolean {
+        var currentTime = System.currentTimeMillis()
+        if (currentTime - creationTime > MILISECONDS_TO_UPDATE) {
+            return true
+        }
+        return false
+    }
+}
