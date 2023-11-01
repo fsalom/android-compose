24f04b07ff01c62d01be43a07af40fe705d15643
Fernando Salom <fer@rudo.es>
2023-11-01 09:02:37 +0100

    Change table name

--- chapter-final/app/src/main/java/com/example/learnwithme/data/datasource/character/database/room/query/CharacterDatabaseQueries.kt
+++ chapter-final/app/src/main/java/com/example/learnwithme/data/datasource/character/database/room/query/CharacterDatabaseQueries.kt
@@ -6,20 +6,21 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.RoomDatabase
 import com.example.learnwithme.data.datasource.character.database.room.dbo.CharacterEntity
-import com.example.learnwithme.manager.database.BaseDao
+import com.example.learnwithme.domain.entity.Character
+import com.example.learnwithme.data.manager.database.BaseDao
 
 @Dao
 abstract class CharacterDao(roomDatabase: RoomDatabase) : BaseDao<CharacterEntity>("characters", roomDatabase) {
-    @Query("SELECT * FROM Characters WHERE isFavorite = 1")
+    @Query("SELECT * FROM characters WHERE isFavorite = 1")
     abstract fun getFavorites(): List<CharacterEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract fun insertOrReplace(character: CharacterEntity)
 
-    @Query("SELECT * FROM Characters WHERE page = :page")
+    @Query("SELECT * FROM characters WHERE page = :page")
     abstract fun getCharacters(page: Int): List<CharacterEntity>
 
-    @Query("SELECT * FROM Characters WHERE name LIKE '%' || :text || '%'")
+    @Query("SELECT * FROM characters WHERE name LIKE '%' || :text || '%'")
     abstract fun searchCharacters(text: String): List<CharacterEntity>
 
 }
