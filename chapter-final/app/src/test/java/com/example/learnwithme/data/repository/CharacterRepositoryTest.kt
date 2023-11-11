d2bc430d4bdae33062515fb2a12dc6ebc7dce931
Fernando Salom <fer@rudo.es>
2023-11-11 10:06:39 +0100

    Add entity tests

--- chapter-final/app/src/test/java/com/example/learnwithme/data/repository/CharacterRepositoryTest.kt
+++ chapter-final/app/src/test/java/com/example/learnwithme/data/repository/CharacterRepositoryTest.kt
@@ -8,15 +8,6 @@ class CharacterRepositoryTest {
     @Test
     fun doAction_getHasNextPageAndCharacters(){
         /* Given */
-        val repositoryTest: CharacterRepository(remo)
-        val mock = mock<RemoteCharactersDataSource> {
-            on { getText() } doReturn "text"
-        }
 
-        /* When */
-        classUnderTest.doAction()
-
-        /* Then */
-        verify(mock).doSomething(any())
     }
 }
